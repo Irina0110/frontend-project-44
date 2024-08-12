06ac91769815c13f5c3d0cf788a691091fff4dd6
Irina0110 <irinasafonova2001@mail.ru>
2024-08-12 09:36:55 +0300

    feat: 3 step

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import * as readlineSync from 'readline-sync';
+
+export const greeting = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
