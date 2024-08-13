2bc1eddcfacea240b024697a27ccd533d6de8489
Irina0110 <irinasafonova2001@mail.ru>
2024-08-13 14:08:06 +0300

    fix: eslint config

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,13 +2,13 @@ import playGame from '../index.js';
 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const generateProgression = (start, step, length) => Array.from({ length }, (_, i) => start + i * step);
+const genProgression = (s, st, len) => Array.from({ length: len }, (_, i) => s + i * st);
 
 const generateRound = () => {
   const start = getRandomNumber(1, 10);
   const step = getRandomNumber(1, 10);
   const length = getRandomNumber(5, 10);
-  const progression = generateProgression(start, step, length);
+  const progression = genProgression(start, step, length);
   const hiddenIndex = getRandomNumber(0, length - 1);
   const correctAnswer = progression[hiddenIndex].toString();
   progression[hiddenIndex] = '..';
