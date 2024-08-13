9a1845da14a1e095dd482556be93347cc4960720
Irina0110 <irinasafonova2001@mail.ru>
2024-08-13 13:23:57 +0300

    feat: 9 step

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Brain-calc  https://asciinema.org/a/vtogZGeFcrgol2J2CVFbaGnf4
 Brain-gcd  https://asciinema.org/a/wew561l15Cb6l52Hdz3GvYVI9
 
 Brain-progression  https://asciinema.org/a/DILrFeBAQw1mDqZYhXRTR6c3b
+
+Brain-prime  https://asciinema.org/a/8r2V6ZoRZ7T0qUid0o3N4PR9l
---
+++ src/games/checkPrime.js
@@ -0,0 +1,24 @@
+import playGame from '../index.js';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+        if (num % i === 0) return false;
+    }
+    return true;
+};
+
+const generateRound = () => {
+    const number = getRandomNumber(2, 100);
+    const question = `${number}`;
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+    return [question, correctAnswer];
+};
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const primeGame = () => playGame(gameDescription, generateRound);
+
+export default primeGame;
