e2ab9f3c6fe6d638ad7a9d2ac8d8e08a2a871793
Irina0110 <irinasafonova2001@mail.ru>
2024-08-13 13:58:21 +0300

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
+import fp from 'eslint-plugin-fp';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
@@ -19,23 +20,35 @@ export default [
     languageOptions: {
       globals: {
         ...globals.node,
+        ...globals.jest,
       },
       parserOptions: {
+        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
+        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
     },
+    plugins: { import: importPlugin, fp },
     rules: {
       ...importPlugin.configs.recommended.rules,
     },
   },
-  ...compat.extends('airbnb'),
+  ...compat.extends('airbnb-base'),
   {
     rules: {
-      'import/extensions': ['error', 'ignorePackages', {
-        js: 'always',
-      }],
-      'no-underscore-dangle': ['error', { allow: ['__dirname', '__filename'] }],
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['__filename', '__dirname'],
+        },
+      ],
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+        },
+      ],
       'import/no-named-as-default': 'off',
       'import/no-named-as-default-member': 'off',
       'no-console': 'off',
